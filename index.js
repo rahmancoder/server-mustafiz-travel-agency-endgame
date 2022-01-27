83e75943fc6dce0793177dced1e062ed200cf9be
Mustafizur <sayhemrahman@gmail.com>
2022-01-27 21:48:33 +0600

    added users blogs

--- index.js
+++ index.js
@@ -37,6 +37,87 @@ async function run() {
         const servicesCollection = database.collection('blogs');
 
 
+
+        // GET API
+
+        app.get('/travel', async (req, res) => {
+            // res.send('Hello World from education')
+            const cursor = travelCollection.find({});
+            const toursimdata = await cursor.toArray();
+            res.send(toursimdata);
+        })
+
+        // GET API FROM Single ID
+        app.get('/travel/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const travel = await travelCollection.findOne(query);
+            // console.log('load user with id: ', id);
+            res.send(travel);
+        })
+
+        // POST API FOR  Add  NEW TravelList
+
+        app.post('/travel', async (req, res) => {
+            const travel = req.body;
+            console.log('hit the post api', travel);
+
+            const result = await travelCollection.insertOne(travel);
+            console.log(result);
+            res.json(result)
+        });
+
+
+
+
+        /*----------------------------------
+          USERS API
+         --------------------------------------------*/
+
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+        })
+
+        // user registration  and google singin save users or bypassing users to mongoDB
+
+        //1. with email password (POST Method useFirebase)
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            console.log(result);
+            res.json(result);
+        });
+
+
+        //2.google singin save users (PUT Method useFirebase)
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        });
+
+        // make admin and admin can add another admin api
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            console.log('put', user);
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: 'admin' } };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result);
+        })
+
+
+
         //GET Blogs API
         app.get('/blogs', async (req, res) => {
             const cursor = productCollection.find({});
